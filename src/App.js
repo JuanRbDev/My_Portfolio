edabb119629af5088ae8f5acf34bd4510192929d
Juan Developer <juanrodriguezrbdev@gmail.com>
2024-08-14 21:15:01 -0500

    Descripción clara de los cambios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -7,15 +7,14 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-
       <Header />
 
       <Routes basename="/My_Portfolio">
+        <Route path="*" element={<Navigate to="/" />} /> {/* Redirige a HomePage en caso de rutas no definidas */}
         <Route path="/" element={<HomePage />} />
       </Routes>
 
       <Footer />
-
     </>
   );
 }
