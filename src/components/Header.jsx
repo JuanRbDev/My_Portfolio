7fed7cbe2c3d61400d376b0873708cc8f1a95b6a
Juan Developer <juanrodriguezrbdev@gmail.com>
2024-08-14 21:07:19 -0500

    Descripción clara de los cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 function Header() {
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                </ul>
+        <header className="header">
+            <nav className="nav-menu">
+                <div className="nav-container">
+                    <ul className="nav-links">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">About Me</Link></li>
+                        <li><Link to="/">Skills</Link></li>
+                        <li><Link to="/">Projects</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                    </ul>
+                </div>
             </nav>
         </header>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
-import banner from '../assets/img/capture.png';
+import React, { useState, useEffect } from 'react';
 import '../css/HomePage.css';
-
 function HomePage() {
+    
+    const [textIndex, setTextIndex] = useState(0);
+    const [displayedText, setDisplayedText] = useState('');
+    const [isAnimating, setIsAnimating] = useState(false);
+    const professions = ['FRONT-END', 'ANDROID'];
+
+    useEffect(() => {
+        let timeout;
+        if (isAnimating) {
+            timeout = setTimeout(() => {
+                setDisplayedText(professions[textIndex]);
+                setIsAnimating(false);
+            }, 1000); // Duración de la animación de escritura
+        } else {
+            timeout = setTimeout(() => {
+                setIsAnimating(true);
+                setTextIndex((prevIndex) => (prevIndex + 1) % professions.length);
+            }, 3000); // Tiempo en que el texto permanece antes de cambiar
+        }
+
+        return () => clearTimeout(timeout);
+    }, [isAnimating, textIndex]);
+
     return (
-        <div className="container">
-            <img src={banner} alt="Yamaha R15" />
+        <div>
+            <div className="container">
+                <div className="box_text">
+                    <h3>Hello, I'm a</h3>
+                    <h1 id='profesion' className={isAnimating ? 'fade-out' : 'fade-in'}>
+                        {displayedText}
+                    </h1>
+                    <h2>DEVELOPER</h2>
+                    <button>About Me</button>
+                </div>
+
+            </div>
         </div>
     );
 }
